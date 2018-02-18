54cce3931b0e05e55d667388915a3a5a53be1406
Connor Jessup <cjessup@umich.edu>
2018-02-17 19:52:40 -0500

    fixed header background issue

--- GuessingGame.js
+++ GuessingGame.js
@@ -109,9 +109,7 @@ $(document).ready(function() {
         $('#title').text("Let's play a game!");
         $('#subtitle').text('Guess a number between 1 and 100!');
         $('.guess').text('-');
-        $('#headers').removeClass('winner');
-        $('#headers').removeClass('loser');
-        $('#headers').removeClass('hint');
+        $('#headers').removeClass('winner loser hint');
         $('#hint, #submit').prop('disabled', false);
         game = new Game();
     });
